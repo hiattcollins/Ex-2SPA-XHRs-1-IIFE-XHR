ed9b701d56e0e283069d309b497e975f0624111f
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-08-06 09:33:39 -0500

    all functions working and code cleaned up

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,77 +1,25 @@
 console.log("iife-xhr.js");
 
-var carnTest;
+function showCarnivores (carnivores) {
 
-// var Predator = (function() {
-
-
-
-	function showCarnivores (carnivores) {
-			
-
-			
-
-			console.log("carnivores/iife", carnivores);
-
-			// document.getElementById("carnivores").innerHTML = `${carnivores}`
-
-			
-			// logCarnTest();
-			// document.getElementById("carnivores").innerHTML = theMeat;
-
-
-		  carnivores.forEach(function (element) {
-		  	document.getElementById("carnivores").innerHTML +=  `<div class="animalCard">
+	carnivores.forEach(function (element) {
+	document.getElementById("carnivores").innerHTML +=  `<div class="animalCard">
 		  														<p>Animal: ${element.Name}</p>
 		  														<p>Habitat: ${element.Habitat}</p>
 		  														</div>`
-		  });
-	}
+	});
+}
 
-	function showHerbivores (herbivores) {
+function showHerbivores (herbivores) {
 
-		
-
-		console.log("herbivores/iife", herbivores);
-
-
-	  herbivores.forEach(function (element) {
-	  	document.getElementById("herbivores").innerHTML +=  `<div class="animalCard">
+	herbivores.forEach(function (element) {
+	document.getElementById("herbivores").innerHTML +=  `<div class="animalCard">
 		  													<p>Animal: ${element.Name}</p>
 		  													<p>Habitat: ${element.Habitat}</p>
 		  													</div>`
-	  });
-	}
+	});
+}
 
-// let tester = Predator.loadCarnivores();
-// console.log("tester", tester);
-
-// // // showCarnivores();
-// Predator.loadCarnivores();
-// Predator.loadHerbivores();
-
-
-// function functionThatDoesStuff (theInput) {
-// 	console.log("theInput", theInput);
-// 	var toPrint = theInput;
-// 	document.getElementById("tester").innerHTML = `${toPrint}`;
-// }
-
-
-
-// var newtest = Predator.sendNotice(functionThatDoesStuff);
-// // document.getElementById("carnivores").innerHTML = `${newtest}`
-// console.log("newtest", newtest);
-
-
-carnTest = Predator.loadCarnivores(showCarnivores);
-console.log("carnTest1", carnTest);
-// console.log("tester", tester);
+Predator.loadCarnivores(showCarnivores);
 
 Predator.loadHerbivores(showHerbivores);
-
-// })();
-
-// function logCarnTest (){
-// 	console.log("carnTest2", carnTest);
-// };
